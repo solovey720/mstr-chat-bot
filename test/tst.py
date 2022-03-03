921b547bc094474631ac6cb81dae96d6f18e0836
taton31 <sima9777@yandex.ru>
2022-03-03 18:43:49 +0300

    final_screen_dashboard

--- test/tst.py
+++ test/tst.py
@@ -5,20 +5,24 @@ from pyppeteer import launch
 #gfk 4E44AA6711EB13AC585C0080EFA5DBEF
 #sales 52969EFC11EA3C7B42930080EF857558
 #obs 0105984311EA440357CD0080EF354C4B
-docID = '0105984311EA440357CD0080EF354C4B'
+docID = '520F150011EB25866E6D0080EF154E9B'
 
 async def main():
     browser = await launch({'headless': True, 'ignoreHTTPSErrors': True, 'defaultViewport': {'width': 1920, 'height': 1080}})
     page = await browser.newPage()
     await page.goto('http://dashboards.corp.mvideo.ru:80/MicroStrategy/servlet/mstrWeb?evt=2048001&src=mstrWeb.2048001&documentID='+ docID +'&currentViewMedia=1&visMode=0&Server=MSTR-IS01.CORP.MVIDEO.RU&Project=%D0%94%D0%B0%D1%88%D0%B1%D0%BE%D1%80%D0%B4%D1%8B%20%D0%BE%D0%BF%D0%B5%D1%80%D1%81%D0%BE%D0%B2%D0%B5%D1%82%D0%B0&Port=0&share=1&uid=administrator&pwd=Ceo143566!@')
-
-    try :
-        await page.waitForSelector('#waitBox > div.mstrmojo-Editor.mstrWaitBox.modal', {'timeout':60000, 'visible': True} )
-        await page.waitForSelector('#waitBox > div.mstrmojo-Editor.mstrWaitBox.modal', {'timeout':60000, 'hidden': True} )
-        await page.waitForSelector('#waitBox > div.mstrmojo-Editor.mstrWaitBox.modal', {'timeout':10000, 'visible': True} )
+    try:
+        await page.waitForSelector('#waitBox > div.mstrmojo-Editor.mstrWaitBox.modal', {'timeout':60000, 'visible': True} ) #ждем ухода самой загрузки документа и появления загрузки данных борда
+        await page.waitForSelector('#waitBox > div.mstrmojo-Editor.mstrWaitBox.modal', {'timeout':60000, 'hidden': True} ) # ждем пока пропадет окно загрузки данных
+        for i in range(5): # Проверяем на фантомную пропажу окна загрузки. Если окно загрузки не появляется 10 сек, делаем скрин и выходим из цикла. иначе ждем менее 60 секунд, пока окно пропадет и возвращаемся в цикл
+            try :
+                await page.waitForSelector('#waitBox > div.mstrmojo-Editor.mstrWaitBox.modal', {'timeout':10000, 'visible': True} )
+            except:
+                await page.screenshot({'path': 'git/mstr-chat-bot/test/example.png'})
+                break
+            await page.waitForSelector('#waitBox > div.mstrmojo-Editor.mstrWaitBox.modal', {'timeout':60000, 'hidden': True} )
     except:
-        await page.screenshot({'path': 'git/mstr-chat-bot/test/example.png'})
-    
+        print('error')
     await browser.close()
 
 
