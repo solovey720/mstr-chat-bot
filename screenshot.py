ba032258301c4a59015a25361b470bb8e79ddfed
taton31 <sima9777@yandex.ru>
2022-03-09 09:47:49 +0300

    func screenshot fin

--- screenshot.py
+++ screenshot.py
@@ -1,5 +1,3 @@
-from aifc import Error
-from distutils.log import error
 from pyppeteer import launch
 import logging
 #http://dashboards.corp.mvideo.ru/MicroStrategy/servlet/mstrWeb?evt=2048001&src=mstrWeb.2048001&documentID=520F150011EB25866E6D0080EF154E9B&currentViewMedia=1&visMode=0&Server=MSTR-IS01.CORP.MVIDEO.RU&Project=%D0%94%D0%B0%D1%88%D0%B1%D0%BE%D1%80%D0%B4%D1%8B%20%D0%BE%D0%BF%D0%B5%D1%80%D1%81%D0%BE%D0%B2%D0%B5%D1%82%D0%B0&Port=0&share=1&uid=administrator&pwd=Ceo143566!@
