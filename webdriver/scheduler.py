d05a8ec8c610835fcf0fc9cd053bfd0e9a018ef5
taton31 <sima9777@yandex.ru>
2022-04-13 18:01:48 +0300

    sched_sema

--- webdriver/scheduler.py
+++ webdriver/scheduler.py
@@ -1,6 +1,11 @@
 from webdriver.screenshot import *
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+import asyncio
+
+run_limit = 5
+
+sem = asyncio.Semaphore(run_limit)
 
 jobstores = {
     'default': SQLAlchemyJobStore(url='sqlite:///database/jobs.sqlite')
@@ -11,7 +16,9 @@ scheduler.start()
 ################################
 scheduler.remove_all_jobs()
 ################################
-#######################################################################Примеры запусков 
+#######################################################################Примеры запусков
+#scheduler.add_job(sem_scheduler_dashboard, "cron", day_of_week='mon-sun', hour=17, minute=27, misfire_grace_time = None, replace_existing=True, args=[user_id, {'docID': '18C63CAE4B8268E07E3DAEA5E275BCC3', 'path_screenshot':f'{user_id}_sec_withsec_withfilt{i}r.png', 'security': ['ACADEMY DINOSAUR', 'ACE GOLDFINGER'],'filters': {'Актер':['PENELOPE','BOB']}}],id=f'{user_id}_sec_withsec_withfil{i}tr', name=f'sec_withsec_wi{i}thfiltr')  
+
 #scheduler.add_job(scheduler_dashboard,  "interval", seconds=1, replace_existing=True, args=[user_id, {'path_screenshot':f'{user_id}_sec_withsec_withfiltr1.png', 'security': ['ACADEMY DINOSAUR', 'ACE GOLDFINGER'],'filters': {'Актер':['PENELOPE','BOB']}}],id=f'{user_id}_sec_withsec_withfiltr1', name='sec_withsec_withfiltr1')
 #scheduler.add_job(scheduler_dashboard, "cron", day_of_week='mon-sun', hour=15, minute=44, misfire_grace_time = None, replace_existing=True, args=[user_id, {'docID': '18C63CAE4B8268E07E3DAEA5E275BCC3', 'path_screenshot':f'{user_id}_sec_withsec_withfiltr.png', 'security': ['ACADEMY DINOSAUR', 'ACE GOLDFINGER'],'filters': {'Актер':['PENELOPE','BOB']}}],id=f'{user_id}_sec_withsec_withfiltr', name=f'sec_withsec_withfiltr')
 
@@ -38,6 +45,11 @@ async def scheduler_dashboard(user_id: int, options=dict()):
     await send_filter_screen(user_id, options = sched_options, new_browser = page)
     await new_browser.close()
     
+async def sem_scheduler_dashboard(user_id: int, options=dict(), i=1):     
+    async with sem:
+        await scheduler_dashboard(user_id, options)
+
+
 def get_user_jobs(user_id: str):
     job_list=[]
     for job in scheduler.get_jobs():
